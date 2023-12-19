1aa168cc91119f8f424f0cedaeefdd9e38915000
Yulia Rayane Nascimento Rodrigues <94014743+yuliaardrigues@users.noreply.github.com>
2023-12-19 00:35:30 +0000

    Sua mensagem de commit aqui

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -1,34 +1,7 @@
-/**
- * Portfolio component
- *
- * Highlights some of  your creations. These can be designs, websites,
- * open source contributions, articles you've written and more.
- *
- * This is a great area for you to to continually add to and refine
- * as you continue to learn and create.
- */
-
 import React from "react";
-
-/**
- * Desk image
- *
- * Below is a sample desk image. Feel free to update this to an image of your choice,
- * updating below imageAltText to string that represents what you see in that image.
- *
- * Need an image? Check out https://unsplash.com to download a photo you
- * freely use on your site.
- */
-import image from "../images/design-desk.jpeg";
 
 const imageAltText = "desktop with books and laptop";
 
-/**
- * Project list
- *
- * An array of objects that will be used to display for your project
- * links section. Below is a sample, update to reflect links you'd like to highlight.
- */
 const projectList = [
   {
     title: "Desenvolvimento de um aplicativo digital wallet,iniciante. ",
@@ -59,27 +32,25 @@ const projectList = [
 const Portfolio = () => {
   return (
     <section className="padding" id="portfolio">
-      <h2 style={{ textAlign: "center" }}>Portfolio</h2>
-      <div style={{ display: "flex", flexDirection: "row", paddingTop: "3rem" }}>
-        <div style={{ maxWidth: "40%", alignSelf: "center" }}>
-          <img
-            src={image}
-            style={{ height: "90%", width: "100%", objectFit: "cover" }}
-            alt={imageAltText}
-          />
-        </div>
-        <div className="container">
-          {projectList.map((project) => (
-            <div className="box" key={project.title}>
-              <a href={project.url} target="_blank" rel="noopener noreferrer">
-                <h3 style={{ flexBasis: "40px" }}>{project.title}</h3>
-              </a>
-              <p className="small">{project.description}</p>
-            </div>
-          ))}
-        </div>
+  <h2 style={{ textAlign: "center" }}>Portfolio</h2>
+  <div style={{ display: "flex", flexDirection: "row", paddingTop: "1rem" }}>
+    <div style={{ maxWidth: "20%", alignSelf: "center", position: "relative" }}>
+      <div style={{ position: "absolute", top: "50%", left: "50%", transform: "translate(-50%, -50%)", color: "#fff" }}>
+        <h3>{imageAltText}</h3>
       </div>
-    </section>
+    </div>
+    <div className="container">
+      {projectList.map((project) => (
+        <div className="box" key={project.title}>
+          <a href={project.url} target="_blank" rel="noopener noreferrer">
+            <h3 style={{ flexBasis: "40px" }}>{project.title}</h3>
+          </a>
+          <p className="small">{project.description}</p>
+        </div>
+      ))}
+    </div>
+  </div>
+</section>
   );
 };
 
